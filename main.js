9c1827acb5cb028e3aba37db15fd048ebb11805d
Mon Álvarez <monserrat23071@gmail.com>
2024-11-26 20:47:43 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+ 
+typewriter.typeString('Desarrolladora Frontend Jr')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('¡Si lo puedes imaginar lo puedes crear!')
+    .pauseFor(2500)
+    // numero caracteres que se borraran (x)
+    .deleteChars(6)
+    .typeString('<strong>programar!</strong>')
+    .pauseFor(2500)
+    .start();
